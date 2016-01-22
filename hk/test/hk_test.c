c73b69ab70c0e8b27cde302b409a2d9004b5037b
ddddddeon <chris@sweetiq.com>
2016-01-22 18:37:16 +0000

    add tests

---
+++ hk/test/hk_test.c
@@ -0,0 +1,11 @@
+#include <hk.h>
+
+int main(int argc, char* argv[]) {
+  hklog(HK_INFO, "everything is fine%s\n", "!");
+  hklog(HK_DEBUG, "just debugging%s\n", "...");
+  hklog(HK_WARN, "i'm warning you%s\n", "...!");
+  hklog(HK_ERR, "error%s\n", "!");
+  hklog(HK_FATAL, "fatal%s\n", "!!");
+
+  return 0;
+}
