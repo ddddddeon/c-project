a102eae5784359feed76e62abe6abde442028752
ddddddeon <chris@sweetiq.com>
2016-01-27 11:57:43 -0500

    use hklog module for output

--- xor/src/main.c
+++ xor/src/main.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
 #include "xor.h"
+
+#include <stdio.h>
+#include <hk/log.h>
+
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("%s\n", "Must provide two numbers!");
+        hklog(HK_ERR, "%s\n", "Must provide two numbers!");
         return 1;
     } else {
         int num1 = atoi(argv[1]);
         int num2 = atoi(argv[2]);
         int num3;
         
-        printf("xor_stateless(%d, %d): %d\n", num1, num2, 
+        hklog(HK_INFO, "xor_stateless(%d, %d): %d\n", num1, num2, 
                xor_stateless(num1, num2));
 
         xor_stateful(&num3, num1, num2);
-        printf("xor_stateful(%d, %d): %d\n", num1, num2, num3);
+        hklog(HK_INFO, "xor_stateful(%d, %d): %d\n", num1, num2, num3);
         
         return 0;
     }
