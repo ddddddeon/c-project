9cdc2707dab1660d6ba17af5f75bfc4a68a76962
ddddddeon <chris@sweetiq.com>
2016-02-19 07:05:00 +0000

    move hk into its own repo

--- hk/src/hk.h
+++
@@ -1,8 +0,0 @@
-#ifndef HK_H
-#define HK_H
-
-#include "log.h"
-#include "bit.h"
-#include "ll.h"
-
-#endif /* !HK_H */
--- hk/test/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef HK_TEST_H
-#define HK_TEST_H
-
-#define HK_PASS 0
-#define HK_FAIL 1
-
-#endif /* !HK_TEST_H */
