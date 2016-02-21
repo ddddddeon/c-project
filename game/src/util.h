616bd316e2b7a1fd9c3d3ed019c02117f3550bea
ddddddeon <chris@sweetiq.com>
2016-02-21 07:28:46 +0000

    untabify util.h

--- game/src/util.h
+++ game/src/util.h
@@ -8,10 +8,10 @@
 
 #define strip_newline(_STR, _LEN) \
     if (_STR[_LEN] == '\n' || _STR[_LEN] == '\r') {\
-	_STR[_LEN] = '\0';\
-	if (_STR[_LEN-1] == '\n' || _STR[_LEN-1] == '\r') {\
-	    _STR[_LEN] = '\0';\
-	}\
+        _STR[_LEN] = '\0';\
+        if (_STR[_LEN-1] == '\n' || _STR[_LEN-1] == '\r') {\
+            _STR[_LEN] = '\0';\
+        }\
     }\
 
 
