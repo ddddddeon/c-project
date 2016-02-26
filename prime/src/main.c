9982d13c5a8fa52405534036e05f21c0e578bb22
ddddddeon <chris@sweetiq.com>
2016-02-26 04:47:22 +0000

    allow the working set to be split between 4 processe

--- prime/src/main.c
+++ prime/src/main.c
@@ -3,10 +3,42 @@
 #include <hk/core.h>
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    unsigned long long int i;
-    for (i=0; i <= ULLONG_MAX; i++) {
+    int num;
+    unsigned long long int i, 
+	                   start, 
+	                   end;
+    
+    if (argc < 1) {
+	num = 0;
+    } else {
+	num = atoi(argv[1]);
+    }
+    
+    switch(num) {
+    case 1:
+	start = 0;
+	end = (ULLONG_MAX / 4) - 1;
+	break;
+    case 2:
+	start = ULLONG_MAX / 4;
+	end = (ULLONG_MAX / 2) - 1;
+	break;
+    case 3:
+	start = ULLONG_MAX / 2;
+	end = ( (ULLONG_MAX / 2) + (ULLONG_MAX / 4) ) - 1;
+	break;
+    case 4:
+	start = ULLONG_MAX / 2 + ULLONG_MAX / 4;
+	end = ULLONG_MAX;;
+	break;
+    default:
+	return HK_NOK;
+    }
+    
+    for (i=start; i <= end; i++) {
 	if (is_prime(i)) {
 	    printf("%llu\n", i);
 	}
