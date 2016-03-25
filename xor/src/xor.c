3ec12def7be247e295652b0ca578392b62185e61
ddddddeon <chris@sweetiq.com>
2016-03-24 21:01:46 -0400

    remove old projects

--- xor/src/xor.c
+++
@@ -1,9 +0,0 @@
-#include "xor.h"
-
-int xor_stateless(int a, int b) {
-    return a ^ b;
-}
-
-void xor_stateful(int *c, int a, int b) {
-    *c = a ^ b;
-}
--- xor/src/xor.h
+++
@@ -1,7 +0,0 @@
-#ifndef XOR_H
-#define XOR_H
-
-int xor_stateless(int, int);
-void xor_stateful(int *, int, int);
-
-#endif /* !XOR_H */
